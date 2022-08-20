6d3d388dd89605141b17fb4f00e143e34f1eb500
codigonaveia <lemosinfotec@gmail.com>
2022-08-20 19:56:12 -0300

    Add project files.

---
+++ canal.codigonaveia.projeto.generateqrcode.UI/Controllers/GenerateQrCodeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace canal.codigonaveia.projeto.generateqrcode.UI.Controllers
+{
+    public class GenerateQrCodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreateQrCode()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateQrCode(string produtoNome)
+        {
+            
+            using(MemoryStream ms = new MemoryStream())
+            {
+                QRCodeGenerator qrGeneraor = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGeneraor.CreateQrCode(produtoNome, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+
+                using(Bitmap qrCodeBitmap = qrCode.GetGraphic(20)){
+
+                    qrCodeBitmap.Save(ms, ImageFormat.Png);
+                    ViewBag.QrCodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                    ViewBag.Produto = produtoNome;
+                }
+
+
+            }
+            return View();
+        }
+    }
+}
---
+++ canal.codigonaveia.projeto.generateqrcode.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace canal.codigonaveia.projeto.generateqrcode.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
